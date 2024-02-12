c1bd5b52ae3856205c1585368f33373232c653fc
rahulr20y <rahulr20y@gmail.com>
2024-02-12 19:25:39 +0530

    Employee Service started

--- employee-sevice/src/main/java/com/learning/microservices/employeesevice/EmployeeSeviceApplication.java
+++ employee-sevice/src/main/java/com/learning/microservices/employeesevice/EmployeeSeviceApplication.java
@@ -3,7 +3,10 @@ package com.learning.microservices.employeesevice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmployeeSeviceApplication {
 
 	public static void main(String[] args) {
