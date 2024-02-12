46d5253e8ad50a08d0619407dd9521dcdd9d2189
rahulr20y <rahulr20y@gmail.com>
2024-02-12 19:11:27 +0530

    Employee Service Initialization

---
+++ employee-sevice/src/main/java/com/learning/microservices/employeesevice/EmployeeSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.microservices.employeesevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSeviceApplication.class, args);
+	}
+
+}
---
+++ employee-sevice/src/test/java/com/learning/microservices/employeesevice/EmployeeSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservices.employeesevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
