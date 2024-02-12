2ad0fdbeab0851c5bbce7441bb0061d1351d6f92
rahulr20y <rahulr20y@gmail.com>
2024-02-12 17:51:19 +0530

    config server initialization

---
+++ config-server/src/main/java/com/learning/microservices/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.learning.microservices.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
